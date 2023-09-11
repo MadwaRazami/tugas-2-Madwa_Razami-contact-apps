9716afd7a02741e18a9e76461887683a77dcddf1
MadwaRazami <120219866+MadwaRazami@users.noreply.github.com>
2023-09-11 15:25:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function viewMenu() { //fungsi untuk menampilkan halaman menu
     console.log("1.Tambah Data \n");
     console.log("2.Lihat Data \n");
     console.log("3.Hapus Data \n");
+    console.log("4.Pencarian Data \n");
+    console.log("5.Hapus Satu Data \n");
     readline.question(`Silahkan Masukan Pilihan Anda  :`, input => {
         mainMenu(Number(input))
     });
@@ -33,6 +35,15 @@ function mainMenu(pilihan) { // fungsi untuk mengatur pilihan menu
         case 2:
             lihatData() 
             break;
+        case 3:
+            resetData()
+            break;
+        case 4:
+            pencarianData()
+            break;
+        case 5:
+            hapusData()
+            break;
         // lanjutkan menu pilihanya disini secara urut
         default:
             console.log("Pilihan Tidak Valid !");
@@ -45,17 +56,42 @@ function mainMenu(pilihan) { // fungsi untuk mengatur pilihan menu
 
 function simpan() { // fungsi untuk menyimpan data
     console.log("Silahkan Masukan Data ! : ");
-    readline.question("Nama :", (nama) => {
-        objectKontak.nama = nama
-        console.log(`Input data berhasil ! :${nama}`);
-        ambilInputanNomor()
-    })
-    
-}
+        readline.question("Nama : ", (nama) => {
+            let nonNumberRegex = /^[^\d]+$/;
+            if(nonNumberRegex.test(nama)){
+                objectKontak.nama = nama;
+                console.log(`Hallo ${nama}, namamu berhasil di input`)
+                ambilInputanNomor();
+            }else{
+                console.log("Masukan nama yang valid");
+                kembali();
+            }
+        })
+    }
 const ambilInputanNomor = () => { // fungsi untuk mengambil inputan nomor
     readline.question("Nomor :", (nomor) => {
-        objectKontak.nomorHp = nomor
-        databaseKontak.push(Object.assign({},objectKontak)) // insert data kedalam array databseKOntak
+       const nomorHP = parseInt(nomor);
+       if(isNaN(nomorHP)){
+       if(isNaN(nomorHP) || objectKontak.nomorHp == nomorHP){
+        console.log("Masukan Nomer HP Anda dengan Angka");
+        kembali();
+       }else if(objectKontak.nomorHp == nomorHP){
+        console.log("Nomor HP yang anda masukan sudah terdata,Mohon masukan nomor HP yang lain");
+        kembali();
+       } else{
+        objectKontak.nomorHp =nomorHP;
+       }
+
+        }
+        else {
+        objectKontak.nomorHp = nomorHP;
+       };
+
+        console.log("Nomer Hp Berhasil di masukan")
+        databaseKontak.push({...objectKontak});
+        // databaseKontak.push(Object.assign({},objectKontak)) // insert data kedalam array databseKOntak
+
+
         kembali()
     })
 }
@@ -76,13 +112,30 @@ function lihatData () { // fungsi untuk melihat list data
 }
 
 function resetData () {
-    // tambahkan fungsi reset  data disini
+    databaseKontak.splice(0, databaseKontak.length);
+    console.log("Data berhasil di reset")
+    kembali()
+    
 }
 
 function pencarianData () {
+    readline.question("Masukan Nama Atau inisial untuk mendapatkan data :" , (i) => {
+        const inisial = i;
+        const searchByInisial = databaseKontak.filter((data) => {
+           return data.nama.includes(inisial);
+        })
+        console.table(searchByInisial);
+        kembali();
+    })
     // tambahkan fungsi pencarian data disini 
 }
 function hapusData () {
+    readline.question("Masukan Index : ",  (index) => {
+        const hpsData = databaseKontak.splice(index, 1);
+        console.log(`Data dengan index ke ${index} sudah terhapus`);
+        console.log(hpsData);
+        kembali();
+    })
     // tambahkan fungsi hapus data data disini 
 }
 
